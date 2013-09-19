93d7696946b97b0f24b1afa83c5216d982aa6c8c
ed <bauchiroad@gmail.com>
2013-09-19 09:31:20 +0000

    imap settings adjusted

--- config/initializers/load_imap_settings.rb
+++ config/initializers/load_imap_settings.rb
@@ -1,9 +1,14 @@
 
-#ImapSettings = YAML.load(File.read(Rails.root + 'config' + 'imap_settings.yml'))[Rails.env]
+ImapSettings = YAML.load(File.read(Rails.root + 'config' + 'imap_settings.yml'))[Rails.env]
 
+#ImapSettings  = File.join(Rails.root, 'config', 'imap_settings.yml')
+
+
+=begin
 ImapSettings =  { 
   host:  ENV['IMAP_HOST'], 
   port:  ENV['IMAP_PORT'], 
   username: ENV['IMAP_USERNAME'], 
   password: ENV['IMAP_PASSWORD'] 
 }
+=end
