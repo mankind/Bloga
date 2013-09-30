153839f25c7e68916357cb1e24f0fab1b4694a45
ed <bauchiroad@gmail.com>
2013-09-30 08:44:41 +0000

    beginning to add methods to NoteBook's controller

--- app/controllers/note_books_controller.rb
+++ app/controllers/note_books_controller.rb
@@ -1,2 +1,18 @@
 class NoteBooksController < ApplicationController
+  before_action :set_note_book, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @note_books = NoteBook.order("created_at desc")
+  end
+  
+  private
+  
+  def note_book_params
+    params.require(:note_book).permit(:name)
+  end
+  
+  def set_note_book
+    @note_book = NoteBook.find(params[:id])
+  end
+  
 end
